23eb634f65dc96db73e454d5fe23e148da0685de
Giovanni Paolo Viganò <gpv.code@gmail.com>
2026-01-21 14:32:54 +0100

    Added chip id 0x70 for compatibility with MPU6500

--- Adafruit_MPU6050.cpp
+++ Adafruit_MPU6050.cpp
@@ -89,9 +89,9 @@ bool Adafruit_MPU6050::begin(uint8_t i2c_address, TwoWire *wire,
 
   Adafruit_BusIO_Register chip_id =
       Adafruit_BusIO_Register(i2c_dev, MPU6050_WHO_AM_I, 1);
-
+  uint32_t id = chip_id.read();
   // make sure we're talking to the right chip
-  if (chip_id.read() != MPU6050_DEVICE_ID) {
+  if (id != MPU6050_DEVICE_ID && id!= MPU6500_DEVICE_ID) {
     return false;
   }
 
